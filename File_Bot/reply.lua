2d7be5e0a82e95f2982c9766058b5568279ebd08
FaNdAmTm <68871726+FaNdAmTm@users.noreply.github.com>
2020-08-01 17:14:45 +0300

    Add files via upload

--- File_Bot/reply.lua
+++ File_Bot/reply.lua
@@ -16,4 +16,27 @@ local texting = {" على الله ❤🤩","الحمدلله ❤☺","تمام 
 send(msg.chat_id_, msg.id_, ''..texting[math.random(#texting)]..'')
 end
 end
-
+if text == "ليش" or text == "ليشش" or text == "ليش😂" or text == "ليش😂😂" or text == "ليش😹" then
+if not database:get(bot_id..'lock:add'..msg.chat_id_) then
+local texting = {" معليكك😂","االشرطة حولو على الجيش😂","حتى احسسن😹","لتدخل😒"}
+send(msg.chat_id_, msg.id_, ''..texting[math.random(#texting)]..'')
+end
+end
+if text == "اكلك" or text == "اكلكك" or text == "اكول" or text == "اككول" or text == "اكول😌" or text == "اكولك" or text == "اكلك😌" or text == "اكلك😹" or text == "اكلك😂" or text == "اكلك😂😂" then
+if not database:get(bot_id..'lock:add'..msg.chat_id_) then
+local texting = {"كول ماكول لححد 😹😹","ااسمعك 👂","احجي😒","اذا طلع شغلةة فاشلةة اكلب التلي عليك 😒"}
+send(msg.chat_id_, msg.id_, ''..texting[math.random(#texting)]..'')
+end
+end
+if text == "شلون" or text == "شلون😂" or text == "شلون😂😂" or text == "ششلون" or text == "ششلون😂" then
+if not database:get(bot_id..'lock:add'..msg.chat_id_) then
+local texting = {"هاذا اللون ونلبس سترة وبنطرون 😹😹","هاذا اللون 😒","احمر اللون 😹","اخضر اللون 😹😹"}
+send(msg.chat_id_, msg.id_, ''..texting[math.random(#texting)]..'')
+end
+end
+if text == "باوع" or text == "شوف" or text == "باوع😹" or text == "باوع😂" or text == "باوع😂😂" then
+if not database:get(bot_id..'lock:add'..msg.chat_id_) then
+local texting = {"شنو ؟😒","👀","اي؟👀","داشوف👀❤"}
+send(msg.chat_id_, msg.id_, ''..texting[math.random(#texting)]..'')
+end
+end
