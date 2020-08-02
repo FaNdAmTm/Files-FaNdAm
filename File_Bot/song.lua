6aebf0c8d93f0ddc264970d64956d5278509ce25
FaNdAmTm <68871726+FaNdAmTm@users.noreply.github.com>
2020-08-02 18:08:04 +0300

    Add files via upload

---
+++ File_Bot/song.lua
@@ -0,0 +1,9 @@
+彡 .[FaNdAm Channel](t.me/fandam0)➢
+]]
+send(msg.chat_id_, msg.id_,(help_text or Text)) 
+return false
+end
+if text == 'م10' then
+local help_text = database:get(bot_id..'help10_text')
+Text = [[
+١-اليسا - عظيمة 
