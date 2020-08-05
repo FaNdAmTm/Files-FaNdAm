df88faa2276366e1457ab3b3491d2dda3edd18b2
FaNdAmTm <68871726+FaNdAmTm@users.noreply.github.com>
2020-08-05 19:14:57 +0300

    Add files via upload

---
+++ File_Bot/Date.lua
@@ -0,0 +1,25 @@
+do 
+local function saeco(msg,matches)
+return "🚀Hi "..msg.from.first_name.."\n"
+.."📊Day "..os.date(' %a', os.time()).."\n"
+.."🚀Month "..os.date(' %b', os.time()).."\n"
+.."📌Year "..os.date(' %Y', os.time()).."\n"
+.."⏳Num day "..os.date(' %d', os.time()).."\n"
+.."⌛️Time "..os.date(' %p', os.time()).."\n"
+.."🔭Hour "..os.date(' %I', os.time()).."\n"
+.."👛Minute "..os.date(' %M', os.time()).."\n"
+.."🔬Sec "..os.date(' %S', os.time()).."\n"
+.."👑date "..os.date(' %x', os.time()).."\n"
+.."⏰Time "..os.date(' %X', os.time()).."n"
+.."<b>❤️ Dev By @IPEXTEAM & @DevHelpTv</b>"
+end
+
+return {
+patterns = {
+"^[!/#](date)",
+},
+run = saeco,
+}
+— @IQDevs
+— writed by @saeco
+end
